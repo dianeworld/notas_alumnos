4a7294602693292cd4753c62cbe09d6f2a4cf503
dianeworld <214973683+dianeworld@users.noreply.github.com.>
2025-10-29 16:55:09 +0100

    Primer commit que muestar la lista de alumnos y sus notas

---
+++ notas.py
@@ -0,0 +1,23 @@
+# Diccionario con alumnos y sus notas
+alumnos = {
+    'Manu': 9.9,
+    'Xavier': 7.2,
+    'Franco': 7,
+    'German': 9,
+    'Alex': 7.4,
+    'Adeel': 7.6,
+    'Oscar': 7,
+    'Andreu': 9,
+    'Javier': 6.9,
+    'Mecano': 8,
+    'Mary': 8.5,
+    'Jose': 7.2,
+    'Diana': 6.8
+}
+
+# Mostrar la lista de alumnos y sus notas
+print('Lista de alumnos y sus notas:')
+for alumno, nota in alumnos.items():
+    print(f'{alumno}: {nota}')
+
+
